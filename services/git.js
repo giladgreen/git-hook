c556075b632bd71cca47fe8f6407af2d23725797
gilad green <gilad.green@autodesk.com>
2023-11-25 18:17:22 +0200

    refactor

--- services/git.js
+++ services/git.js
@@ -2,8 +2,15 @@ const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
 
-async function process({ payload }){
-  console.log('## got process for:', payload.action);
+async function process({ payload: { action, label, pull_request} }){
+
+  console.log('## got process for:', action);
+  console.log('## label:', label?.name);
+  console.log('## pull_request:', pull_request?.url);
+  console.log('## state:', pull_request?.state);
+  console.log('## title:', pull_request?.title);
+  console.log('## by:', pull_request?.user?.login);
+
   // const result = await db.query(
   //   `INSERT INTO programming_languages
   //   (name, released_year, githut_rank, pypl_rank, tiobe_rank)
