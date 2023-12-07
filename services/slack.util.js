90f3328923dbf9f781dcab18243fc20cbd34512c
gilad green <gilad.green@autodesk.com>
2023-12-07 17:39:52 +0200

    refactor

--- services/slack.util.js
+++ services/slack.util.js
@@ -108,7 +108,8 @@ const sendSlackMessage = async (message) => {
 
                 return result.scheduled_message_id;
             } catch (e) {
-                console.error('# failed to send schedule message:', e);
+                console.error('# failed to send schedule message. error message:', e.message);
+                console.error('# failed to send schedule message. error data:',JSON.stringify(e.data));
                 return await sendSlackMessageNow(message, channel);
             }
         }
